46eaf5d645204e197d43641a4dfcef039e014c04
Raul <raul.alhena@gmail.com>
2023-08-07 11:23:57 +0200

    all tests pass

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+export const add = (a, b) => {
+    return a + b;
+}
+
+export const sub = (a, b) => {
+    return a - b;
+}
+
+export const mul = (a, b) => {
+    return a * b;
+}
+
+export const div = (a, b) => {
+    return a / b;
+}
+
+export const mod = (a, b) => {
+    return a % b;
+}
---
+++ app.test.js
@@ -0,0 +1,34 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import { add, sub, mul, div, mod } from './app.js';
+
+
+test('add should return a + b', () => {
+    const a = 1;
+    const b = 1;
+    assert.strictEqual(add(a, b), 2);
+});
+
+test('sub should return a - b', () => {
+    const a = 1;
+    const b = 1;
+    assert.strictEqual(sub(a, b), 0);
+});
+
+test('mul should return a * b', () => {
+    const a = 2;
+    const b = 2;
+    assert.strictEqual(mul(a, b), 4);
+});
+
+test('div should return a / b', () => {
+    const a = 4;
+    const b = 2;
+    assert.strictEqual(div(a, b), 2);
+});
+
+test('mod should return a % b', () => {
+    const a = 4;
+    const b = 2;
+    assert.strictEqual(mod(a, b), 0);
+});
